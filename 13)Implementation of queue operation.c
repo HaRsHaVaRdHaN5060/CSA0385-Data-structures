bd36b2b306e98f594148a511c2db937fa9546273
Devsachin2003 <112550872+Devsachin2003@users.noreply.github.com>
2022-10-06 10:30:31 +0530

    Update 13)Implementation of queue operation.c

--- 13)Implementation of queue operation.c
+++ 13)Implementation of queue operation.c
@@ -12,10 +12,10 @@ int main()
 int choice;
 while (1)
 {
-printf("1.Insert element to queue n");
-printf("2.Delete element from queue n");
-printf("3.Display all elements of queue n");
-printf("4.Quit n");
+printf("1.Insert element to queue \n");
+printf("2.Delete element from queue \n");
+printf("3.Display all elements of queue \n");
+printf("4.Quit \n");
 printf("Enter your choice : ");
 scanf("%d", &choice);
 switch(choice)
@@ -32,7 +32,7 @@ break;
 case 4:
 exit(1);
 default:
-printf("Wrong choice n");
+printf("Wrong choice \n");
 }
 }
 }
@@ -40,7 +40,7 @@ void insert()
 {
 int item;
 if(rear == MAX - 1)
-printf("Queue Overflow n");
+printf("Queue Overflow \n");
 else
 {
 if(front== - 1)
@@ -55,12 +55,12 @@ void delete()
 {
 if(front == - 1 || front > rear)
 {
-printf("Queue Underflow n");
+printf("Queue Underflow \n");
 return;
 }
 else
 {
-printf("Element deleted from queue is : %dn", queue_array[front]);
+printf("Element deleted from queue is : %d\n", queue_array[front]);
 front = front + 1;
 }
 }
@@ -68,12 +68,12 @@ void display()
 {
 int i;
 if(front == - 1)
-printf("Queue is empty n");
+printf("Queue is empty \n");
 else
 {
-printf("Queue is : n");
+printf("Queue is : \n");
 for(i = front; i <= rear; i++)
 printf("%d ", queue_array[i]);
-printf("n");
+printf("\n");
 }
 }
